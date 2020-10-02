31f4203aefc0bafd3a3aa28973fe10c499fd1b2f
flashton2003 <flashton@gmail.com>
2020-10-02 09:49:26 +0200

    v1 working

--- query_ena_api.py
+++ query_ena_api.py
@@ -10,17 +10,9 @@ def main():
     root = tree.getroot()
     # print(root.attrib)
     for child in root.iter():
-        # print(child.tag, child.text)
         if child.text.startswith('ERR'):
             print(child.text)
-        # print(dir(child))
-        # if len(child) > 4:
-            # print(child[4].tag, child[4].attrib)
-        # print(child.tag, child.attrib)
-    # for child in root.findall('ENA-RUN'):
-        # print(child.tag, child.attrib)
 
-    # pass
 
 if __name__ == '__main__':
     main()

1d3b3697a29fddcb862cfe7bb17a5b5abc13cd66
flashton2003 <flashton@gmail.com>
2020-10-02 10:24:18 +0200

    changes

--- query_ena_api.py
+++ query_ena_api.py
@@ -13,19 +13,28 @@ def query_api_parse_response(ena_accession):
     response = requests.get(f'https://www.ebi.ac.uk/ena/browser/api/xml/{ena_accession}')
     return response
 
-def parse_xml_get_err(response):
+def parse_xml_get_err(response, ena_accession):
     tree = ET.ElementTree(ET.fromstring(response.content))
-    # print(vars(tree))
     root = tree.getroot()
-    # print(root.attrib)
     for child in root.iter():
-        if child.text.startswith('ERR'):
-            print(child.text)
+        try:
+            ## the text attribute is the one I want
+            if child.text.startswith('ERR'):
+                print(ena_accession, child.text, sep = '\t')
+                ## there should only be one ERR per file so can do this
+                return
+        except AttributeError:
+            pass
+    ## if get to the end of the file and not returned, then no hits
+    print(f'No ERR record for {ena_accession}')
 
 def main():
+    '''
+    Example output https://www.ebi.ac.uk/ena/browser/api/xml/ERS235824
+    '''
     ena_accession = get_args()
     response = query_api_parse_response(ena_accession)
-    parse_xml_get_err(response)
+    parse_xml_get_err(response, ena_accession)
 
 if __name__ == '__main__':
     main()

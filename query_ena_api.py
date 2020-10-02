224cc018d6d8008edf3ac9f0ec65cddbce194b56
flashton2003 <flashton@gmail.com>
2020-10-02 09:59:45 +0200

    working, tidied up

--- query_ena_api.py
+++ query_ena_api.py
@@ -1,10 +1,19 @@
+import sys
 import requests
 import xml.etree.ElementTree as ET
 
-def main():
-    response = requests.get('https://www.ebi.ac.uk/ena/browser/api/xml/ERS235824')
-    # print(vars(response))
-    # print(response.content)
+def get_args():
+    if len(sys.argv) != 2:
+        print('Usage: python query_ena_api.py <ENA_ACCESSION>')
+        sys.exit()
+    else:
+        return sys.argv[1]
+
+def query_api_parse_response(ena_accession):
+    response = requests.get(f'https://www.ebi.ac.uk/ena/browser/api/xml/{ena_accession}')
+    return response
+
+def parse_xml_get_err(response):
     tree = ET.ElementTree(ET.fromstring(response.content))
     # print(vars(tree))
     root = tree.getroot()
@@ -13,6 +22,10 @@ def main():
         if child.text.startswith('ERR'):
             print(child.text)
 
+def main():
+    ena_accession = get_args()
+    response = query_api_parse_response(ena_accession)
+    parse_xml_get_err(response)
 
 if __name__ == '__main__':
     main()
